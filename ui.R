c7a52f87fa78085a5d0889894051a12e72139569
juanponsg <73911559+juanponsg@users.noreply.github.com>
2021-02-18 20:04:02 +0100

    ACCTUALIZADO 18.02.2021

--- ui.R
+++ ui.R
@@ -20,9 +20,7 @@ library(shinyalert)
 shinyUI(fluidPage(
     theme = shinytheme("superhero"),
     
-    # Application title
-    #titlePanel("Estudia e Investiga 2020-2021"),
-    #img(src='Logo.png', align = "left"),
+    # TITULO Y LOGO
     titlePanel( div(column(width = 8, height = 8, h1("Estudia e Investiga 2020-2021")),
                     column(width = 4, height = 6, tags$img(src='Logo.png'))),
                 windowTitle="Estudia e Investiga 2020-2021"),
@@ -31,18 +29,20 @@ shinyUI(fluidPage(
     # Sidebar with a slider input for number of bins
     sidebarLayout(
         sidebarPanel(
-            tags$p("A continuació anem a comentar una sèrie de casos amb  Proves Diagnòstiques d’Infecció Activa, més conegut com PDIA, positives en la Comunitat Valenciana, segons data de diagnòstic de laboratori. Les PDIA inclouen dues proves de detecció d’infecció activa, la PCR (sigles de polymerase chain reaction, reacció en cadena de la polimerasa) i el test d’antígens, d’acord amb els procediments i documents tècnics vigents elaborats pel Ministeri de Sanitat."),
+            # INTRO
+            tags$p("A continuaciÃ³ anem a comentar una sÃ¨rie de casos amb  Proves DiagnÃ²stiques dâInfecciÃ³ Activa, mÃ©s conegut com PDIA, positives en la Comunitat Valenciana, segons data de diagnÃ²stic de laboratori. Les PDIA inclouen dues proves de detecciÃ³ dâinfecciÃ³ activa, la PCR (sigles de polymerase chain reaction, reacciÃ³ en cadena de la polimerasa) i el test dâantÃ­gens, dâacord amb els procediments i documents tÃ¨cnics vigents elaborats pel Ministeri de Sanitat."),
             
+            # BOTON DE SELEECION DE LA ZONA
             selectInput (inputId = "Zona",
                          label = "ZONA:",
                          choices = c("C.Valenciana" = "C.Valenciana"
                                      , "Homes" = "Homes/Hombres"
                                      , "Dones" = "Dones/Mujeres"
                                      , "Prov. Alacant" = "Prov. Alacant/Alicante"
-                                     , "Prov. Castelló" = "Prov. Castelló/Castellón"
-                                     , "Prov. València" = "Prov. València"
+                                     , "Prov. CastellÃ³" = "Prov. CastellÃ³/CastellÃ³n"
+                                     , "Prov. ValÃ¨ncia" = "Prov. ValÃ¨ncia"
                                      , "Dep. Vinaros" = "DEPARTAMENT DE SALUT DE VINAROS"
-                                     , "Dep. Castelló" = "DEPARTAMENT DE SALUT DE CASTELLO"
+                                     , "Dep. CastellÃ³" = "DEPARTAMENT DE SALUT DE CASTELLO"
                                      , "Dep. La Plana" = "DEPARTAMENT DE SALUT DE LA PLANA"
                                      , "Dep. Sagunt" = "DEPARTAMENT DE SALUT DE SAGUNT"
                                      , "Dep. VCIA Clinic-Malva Rosa" = "DEPARTAMENT DE SALUT DE VCIA CLINIC-LA MALVA-ROSA"
@@ -67,35 +67,36 @@ shinyUI(fluidPage(
                                      , "Dep. Elx-Crevillent"= "DEPARTAMENT DE SALUT D'ELX-CREVILLENT"
                          ) 
             ), 
-            tags$p("Última data d'actualització :",data_actualitzacio,".")
+            # FECHA ACTUALIZACION
+            tags$p("Ãltima data d'actualitzaciÃ³ :",data_actualitzacio,"."),
+            
+            # ENLACE AL COGIDO
+            tags$a(href="https://github.com/juanponsg/Estudia-e-Investiga.git", "CÃ³digo")
         ),
         
         # Show a plot of the generated distribution
         mainPanel(
             useShinyalert(),
             
+            # MOSTRAR WIDGETS EN UNA FILA CON TRES COLUMNAS Y SUS CORRESPONDIENTES BOTONES DE INFORMACION
             fluidRow(
                 div(column(4,div(column(3, tags$h4("Mitjana")),column(1,actionButton("btn1", (tags$img(src="info.png",width = 24, height = 24))))), flexdashboard::gaugeOutput("mitjana")),
                     column(4,div(column(4, tags$h4("Incidencia")),column(1,actionButton("btn2", (tags$img(src="info.png",width = 24, height = 24))))), flexdashboard::gaugeOutput("incidencia")),
-                    column(4,div(column(7, tags$h4("Mitjana 14 dies")),column(1,actionButton("btn3", (tags$img(src="info.png",width = 24, height = 24))))), flexdashboard::gaugeOutput("mitjana14"))
-                    #column(4,tabsetPanel(tabPanel(tags$h4("Mitjana"), flexdashboard::gaugeOutput("mitjana")),tabPanel("DescripciÃÂÃÂ³", "Mitjana de", textOutput("departamento1"), "respecte a la mitjana de la Comunitat Valenciana"))),
-                    #column(4,tabsetPanel(tabPanel(tags$h4("Incidencia"), flexdashboard::gaugeOutput("incidencia")),tabPanel("DescripciÃÂÃÂÃÂÃÂ³", "Incidencia acumulada en els Ultims 14 dies a", textOutput("departamento2")))),
-                    #column(4,tabsetPanel(tabPanel(tags$h4("Mitjana 14 dies"), flexdashboard::gaugeOutput("mitjana14")),tabPanel("DescripciÃÂÃÂÃÂÃÂ³", "Mitjana dels Ultims 14 dies de", textOutput("departamento3"), "respecte a la mitjana dels Ultims 14 dies de la Comunitat Valenciana"))),
-                )
+                    column(4,div(column(7, tags$h4("Mitjana 14 dies")),column(1,actionButton("btn3", (tags$img(src="info.png",width = 24, height = 24))))), flexdashboard::gaugeOutput("mitjana14")))
             ),
             
+            # MOSTRAR TITULOS MAPA Y GRAFICA CON LOS BOTONES DE INFORMACION EN UNA LINEA Y DOS COLUMNAS
             fluidRow(
                 div(column(6,div(column(5, tags$blockquote("Mapa")),column(1,actionButton("btn4", (tags$img(src="info.png",width = 24, height = 24)))) ) ),
-                    column(6,div(column(5, tags$blockquote("Evolució")),column(1,actionButton("btn5", (tags$img(src="info.png",width = 24, height = 24))))))
-                    #column(6,tabsetPanel(tabPanel(tags$blockquote("Mapa")), tabPanel("DescripcÃÂÃÂ³", "Mapa de la Comunitat Valenciana dividt per els departaments de salud.", tags$br(), "EL mapa esta acolorit segons la Incidencia en cada departament.", tags$br(), "A mes ens indica la localitzacio del departament i la Incidencia actual."))),
-                    #column(6,tabsetPanel(tabPanel(tags$blockquote("EvoluciÃÂÃÂÃÂÃÂ³")), tabPanel("DescripciÃÂÃÂ³", "Grafica de l'evoluciÃÂÃÂ³ dels casos positius de Covid-19 des del 2020-01-02 fins ", data_actualitzacio)))
-                )
+                    column(6,div(column(5, tags$blockquote("EvoluciÃ³")),column(1,actionButton("btn5", (tags$img(src="info.png",width = 24, height = 24)))))))
             ),
             
+            # MOSTRAR EL MAPA Y LA GRAFICA EN UNA MISMA FILA Y DOS COLUMNAS
             fluidRow(
                 div(column(6, plotOutput("mapa")),
                     column(6, plotOutput("evolucion")), align = "center")
             )
+            
             
         )
     )
